f20e8623411a65125a9aab03904b07207e3a6cd0
aditya4114 <aditya04112002@gmail.com>
2025-07-19 18:13:16 +0530

    Deletion

--- companyms/companyms/src/main/java/com/project/companyms/company/CompanyRepository.java
+++
@@ -1,6 +0,0 @@
-package com.project.companyms.company;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company,Long> {
-}
